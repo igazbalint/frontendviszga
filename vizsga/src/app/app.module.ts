7c234672e8d9875ae37740cb367c721ac7ec1e72
Tsukiyadesu <124679441+Tsukiyadesu@users.noreply.github.com>
2023-03-20 12:37:47 +0100

    exam comp

--- vizsga/src/app/app.module.ts
+++ vizsga/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExamComponent } from './exam/exam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamComponent
   ],
   imports: [
     BrowserModule
