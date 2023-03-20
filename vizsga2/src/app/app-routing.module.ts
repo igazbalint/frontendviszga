17efbaac6daba93576886c81989ac17bdbb18771
Tsukiyadesu <124679441+Tsukiyadesu@users.noreply.github.com>
2023-03-20 13:03:02 +0100

    exam route

--- vizsga2/src/app/app-routing.module.ts
+++ vizsga2/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExamComponent } from './exam/exam.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'exam',component:ExamComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- vizsga2/src/app/app.module.ts
+++ vizsga2/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExamComponent } from './exam/exam.component';
+
 
 @NgModule({
   declarations: [
