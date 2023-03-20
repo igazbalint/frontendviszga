0713deea51c60353814f8b871b41243b283ee365
Tsukiyadesu <124679441+Tsukiyadesu@users.noreply.github.com>
2023-03-20 14:19:57 +0100

    log display

--- vizsga2/src/app/exam/exam.component.ts
+++ vizsga2/src/app/exam/exam.component.ts
@@ -4,12 +4,28 @@ import { FormsModule } from '@angular/forms'
 @Component({
   selector: 'app-exam',
   templateUrl: './exam.component.html',
-  styleUrls: ['./exam.component.scss']
+  styleUrls: ['./exam.component.scss'],
 })
 export class ExamComponent {
   text!: string;
-  result: string =''
-  saveResult(){
-    this.result = this.text
+  result: string = '';
+  results: string[]= [];
+  isEven!: boolean;
+
+  decide() {
+    let number = parseInt(this.text);
+    if(isNaN(number)){console.error('adjon meg egy szamot')}
+    if (number % 2 == 0) {
+      this.isEven = true;
+    } else this.isEven = false;
+
+    console.log(this.isEven);
+  }
+  saveResult() {
+    this.result = this.text;
+    //this.results.push(this.text);
+    //console.log(this.results);
+    this.results.push(this.text);
+    console.log(this.results)
   }
 }
